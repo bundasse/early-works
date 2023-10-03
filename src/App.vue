06c0e1f0aabe87b609ad687a02e300e7aa6bc3b8
bundasse <ymtotolove@gmail.com>
2023-10-03 16:12:17 +0900

    1003 내비게이션, 푸터

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,22 @@
     </div>
   </div>
   <!-- 내비게이션 -->
+  <div class="max-w-6xl mx-auto py-10">
+    <ul class="flex justify-between">
+      <li v-for="(e, i) in mainmenu" :key="i" class="basis-[32.5%]">
+        <p class="pb-3 text-sm font-bold border-b mb-3">
+          {{ e.name }}
+        </p>
+        <ul class="flex justify-between flex-wrap">
+          <li v-for="(el, index) in submenu[i]" :key="index" class="basis-[47%]">
+            <p class="py-2 text-sm text-gray-600 hover:text-black">
+              {{ el }}
+            </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
   <!-- //내비게이션 -->
   <!-- //헤더 -->
   <!-- 비주얼 -->
@@ -70,39 +86,41 @@
     </article>
   </section>
   <!-- 푸터 -->
-  <div>
-    <div>
-      <h5>커뮤니티</h5>
-      <div>
-        <p><a href="#" @click.prevent>공지사항</a></p>
-        <p><a href="#" @click.prevent>이벤트</a></p>
-        <p><a href="#" @click.prevent>자주묻는 질문</a></p>
-        <p><a href="#" @click.prevent>1:1문의</a></p>
+  <div class="border-t py-10">
+    <div class="max-w-6xl mx-auto flex justify-between">
+      <div class="basis-[25%]">
+        <h5 class="text-xs text-gray-600 font-bold mb-5">커뮤니티</h5>
+        <div class="w-2/3 flex flex-wrap gap-y-3 mb-5">
+          <p class="basis-1/2 text-xs text-gray-600"><a href="#" @click.prevent>공지사항</a></p>
+          <p class="basis-1/2 text-xs text-gray-600"><a href="#" @click.prevent>이벤트</a></p>
+          <p class="basis-1/2 text-xs text-gray-600"><a href="#" @click.prevent>자주묻는 질문</a></p>
+          <p class="basis-1/2 text-xs text-gray-600"><a href="#" @click.prevent>1:1문의</a></p>
+        </div>
+        <ul class="flex gap-3">
+          <li>카카오톡</li>
+          <li>인스타</li>
+          <li>페북</li>
+        </ul>
       </div>
-      <ul>
-        <li>카카오톡</li>
-        <li>인스타</li>
-        <li>페북</li>
-      </ul>
-    </div>
-    <div>
-      <h5>협력제안</h5>
-      <p>집반찬연구소와 함께하고 싶으신 분!<br/><span>환영합니다</span></p>
-      <span>생산자 / 공급업체 / 봉사단체 / 구직자</span>
-      <p>zipbanchan@sannamchon.co.kr</p>
-    </div>
-    <div>
-      <h5>고객센터</h5>
-      <p>1588-3754</p>
-      <p>
-        <span>월~토요일 08:30 ~ 17:30</span>
-        <span>점심시간 12:00 ~ 13:00</span>
-        <span>일요일, 공휴일은 휴무입니다.</span>
-      </p>
+      <div class="basis-[33%]">
+        <h5 class="text-xs text-gray-600 font-bold mb-5">협력제안</h5>
+        <p class=text-lg>집반찬연구소와 함께하고 싶으신 분!<br/><span class="font-bold">환영합니다</span></p>
+        <span class="text-xs text-gray-600">생산자 / 공급업체 / 봉사단체 / 구직자</span>
+        <p class="mt-3 text">zipbanchan@sannamchon.co.kr</p>
+      </div>
+      <div class="basis-[40%]">
+        <h5 class="text-xs text-gray-600 font-bold mb-5">고객센터</h5>
+        <p class=" text-2xl font-bold mb-5">1588-3754</p>
+        <p class="flex flex-col text-sm text-gray-600">
+          <span>월~토요일 08:30 ~ 17:30</span>
+          <span>점심시간 12:00 ~ 13:00</span>
+          <span>일요일, 공휴일은 휴무입니다.</span>
+        </p>
+      </div>
     </div>
   </div>
   <div class="w-full bg-[#f7f7f7]">
-    <div class="max-w-7xl mx-auto text-xs py-[30px]">
+    <div class="max-w-6xl mx-auto text-xs py-[30px]">
       <ul class="flex gap-4">
         <li class="relative after:absolute after:w-[1px] after:h-[14px] after:top-0 after:-right-2 after:bg-gray-500"><a href="#" @click.prevent>집반찬연구소 소개</a></li>
         <li class="relative after:absolute after:w-[1px] after:h-[14px] after:top-0 after:-right-2 after:bg-gray-500"><a href="#" @click.prevent>이용약관</a></li>
@@ -135,7 +153,11 @@ export default {
   name: 'App',
   data() {
     return {
-      addressOpen: false
+      addressOpen: false,
+      mainmenu:[{name:'모든반찬',link:''},{name:'간편검색',link:''},{name:'인기검색',link:''}],
+      submenu:[['추석 차례상', '2023 추석', '아이 반찬 6종, 등장!', '특선! 비빔밥 재료', '정기배송', '무침', '볶음', '조림', '어린이 반찬', '요리놀이터', '국/찌개/탕', '소분야채', '메인요리', '묶음반찬', '전/생선', '협업상품', '김치/절임/젓갈', '곡류/양념', '대용량', '예치금'],
+      ['#신제품', '#인기', '#냉동', '#맵지않음', '#따듯하게먹는반찬', '#조리필요', '#제철반찬', '#차갑게먹는반찬'],
+      ['#할인반찬', '#어린이반찬', '#부모님반찬', '#소분야채', '#제철반찬', '#반조리', '#쿠킹박스']]
     }
   },
   components: {
