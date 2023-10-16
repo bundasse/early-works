963813a7187795a5d34f5bb7e4b4f6f825d05057
bundasse <ymtotolove@gmail.com>
2023-10-16 21:10:46 +0900

    1016 주간추천반찬 완료

--- src/App.vue
+++ src/App.vue
@@ -86,11 +86,22 @@
           <li v-for="(e, i) in category" :key="i" @click="selectedCategory = e.link" class="text-sm px-4 py-2 rounded-md cursor-pointer" :class="selectedCategory === e.link ? ' bg-green-900 text-white':'bg-gray-200 text-gray-600'">{{ e.name }}</li>
         </ul>
         <ul class="flex justify-between">
-          <li v-for="(e, i) in cateItem" :key="i" class="basis-[24%]">
-            <img :src="require(`@/assets/images/`+e.img+`.jpg`)" alt="">
-            <div class="bg-white p-5">
-              <p class="text-xl font-bold line-clamp-1 mb-5">{{ e.title }}</p>
-              <span class="text-xs text-gray-600 line-clamp-3">{{ e.desc }}</span>
+          <li v-for="(e, i) in weeklyItem[selectedCategory]" :key="i" class="basis-[24%]">
+            <div class="cursor-pointer">
+                <div class="relative">
+                    <img :src="require(`@/assets/images/`+e.img+`.jpg`)" alt="">
+                    <p v-if="e.sale !== 0" class="absolute w-[50px] h-[50px] bg-red-600 text-white top-4 left-4 z-5 py-3 text-center font-bold">{{ e.sale }}%</p>
+                    <p class="absolute w-[50px] h-[50px] bg-black opacity-50 bottom-4 right-4 z-5 p-[6px]"><img :src="require(`@/assets/icon/cart-white.svg`)" class="w-10" alt=""></p>
+                </div>
+                <div class="bg-white py-5">
+                    <ul class="flex gap-2 h-4">
+                        <li v-for="(el, index) in e.tag" :key="index" class="text-xs" :class="el === '인기'? 'text-red-600': 'text-gray-600'">#{{ el }}</li>
+                    </ul>
+                    <p class="font-bold my-1">{{ e.name }}</p>
+                    <p class="text-xs text-gray-600 mb-2">{{ e.desc }}</p>
+                    <p class="text-red-600 font-bold text-lg">{{  salePrice(e.sale, e.price) }}<span clas="text-sm">원</span></p>
+                    <span v-if="e.sale !== 0" class="text-gray-400 text-xs line-through">{{e.price}}원</span>
+                </div>
             </div>
           </li>
         </ul>
@@ -208,13 +219,6 @@ export default {
     salePrice(sale, price){
       const cal =  Math.round((100-Number(sale))*Number(price)/10000)*100;
       return cal
-    }
-  },
-  computed:{
-    cateItem(){
-      return this.weeklyItem.filter((data)=>{
-          return data === this.selectedCategory
-      })
     }
   },
   setup() {
